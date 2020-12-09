2597a22f32e47d445bf0bbfa6924e18f4eaedb47
adeSugiono <adesugiono354@gmail.com>
2020-12-09 22:50:13 +0700

    first commit

---
+++ server/app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+mongoose.connect('mongodb://localhost/chatdb', {useNewUrlParser: true});
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/chat');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors());
+
+app.use('/', indexRouter);
+app.use('/api/chat', usersRouter);
+
+module.exports = app;
