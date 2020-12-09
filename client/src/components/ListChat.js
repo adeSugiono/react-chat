d2fa39db66cc514b87ea5ed78646e4070a24b625
adeSugiono <adesugiono354@gmail.com>
2020-12-09 22:57:20 +0700

    add File new

---
+++ client/src/components/ListChat.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ListChat(props) {
+    return (
+        <div className="card text-center">
+            <h2>React Chat</h2>
+        </div>
+    )
+}
---
+++ client/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import BoxChat from './components/BoxChat';
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <BoxChat />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
