d5075aa0260e5e44c877634709a6ef65e4acd0b1
adeSugiono <adesugiono354@gmail.com>
2020-12-09 22:56:19 +0700

    fixed untracked files 2

--- client/src/components/ListChat.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function ListChat(props) {
-    return (
-        <div className="card text-center">
-            <h2>React Chat</h2>
-        </div>
-    )
-}
--- server/models/chatdata.js
+++
@@ -1,9 +0,0 @@
-const mongoose =require('mongoose');
-
-const dataSchema = new mongoose.Schema({
-    id: Number,
-    name: String,
-    chats: String
-});
-
-module.exports = mongoose.model('Data', dataSchema);
